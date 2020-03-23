8050b1e95a888fe2b84ecf05d249d0da0dbd6554
Bernie White <bewhite@microsoft.com>
2020-03-23 13:37:55 +1000

    Update PSRule module to v0.16.0-B2003027

--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -195,8 +195,8 @@ task PSScriptAnalyzer NuGet, {
 
 # Synopsis: Install PSRule
 task PSRule NuGet, {
-    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion '0.16.0-B2003022' -AllowPrerelease -ErrorAction Ignore)) {
-        Install-Module -Name PSRule -MinimumVersion '0.16.0-B2003022' -AllowPrerelease -Scope CurrentUser -Force;
+    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion '0.16.0-B2003027' -AllowPrerelease -ErrorAction Ignore)) {
+        Install-Module -Name PSRule -MinimumVersion '0.16.0-B2003027' -AllowPrerelease -Scope CurrentUser -Force;
     }
     Import-Module -Name PSRule -Verbose:$False;
 }
